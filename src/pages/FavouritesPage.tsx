2c3aaa41a2faf2b5fbfa124f0cf42e5e37eeae92
Damian <errollgarner@yandex.ru>
2023-01-08 11:17:48 +0100

    Work with State(actions, reducer) & FavoritePage(add & remove github url to favourites)

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {useAppSelector} from '../hooks/redux'
 
 export function FavouritesPage() {
-    return(
-        <h1>/favourites</h1>
+
+    const {favourites} = useAppSelector(state => state.github)
+
+    if (favourites.length === 0) return <p className={'text-center'}>No items in Favourites</p>
+
+    return (
+        <div className={'flex justify-center pt-10 mx-auto h-screen w-screen'}>
+            <ul className={'list-none'}>
+                {favourites.map(i => (
+                    <li key={i}>
+                        <a href={i} target={'_blank'}>{i}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
